5adedecc91938cd85bd97c428dea33257c0b37cb
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-02 16:36:02 -0400

    add start and reset buttons + div containers for player's turn and round scores

--- code.js
+++ code.js
@@ -41,7 +41,7 @@ function createPlayer(name){
 
 
 // Factor function to control gameplay using gameBoard and player objects
-function startGame(p1, p2){
+const gameController = function startGame(){
     let isPlayerOneTurn = true;
 
     // updates gameBoard.array for each move
@@ -83,13 +83,119 @@ function startGame(p1, p2){
         isPlayerOneTurn = true;
     }
 
-    return { handleTurn, getResult, reset};
+    return {handleTurn, getResult, reset, isPlayerOne: () => isPlayerOneTurn};
+}();
+
+
+
+
+// display array contents to website
+const displayBoard = function(array){
+
+    const board = document.querySelector("#board");
+
+    function render(array){
+        for (let i = 0; i < array.length; i++){
+            const cell = board.children[i];
+            let entry = array[i];
+    
+            if (entry == "player1"){
+                cell.textContent = "X";
+            }else if(entry == "player2"){
+                cell.textContent = "O";
+            }else{
+                cell.textContent = "";
+            }
+        }
+    }
+
+    return{render};
+
+}();
+
+// function to add event listeners
+let player1;
+let player2;
+let round = 1;
+
+
+function setupEventListeners(){
+
+    const board = document.querySelector("#board");
+    const buttons = document.querySelector("#buttons");
+    let gameStarted = false;
+
+
+
+    board.addEventListener("click", e => {
+        if (!gameStarted) return;
+        let indexClicked = parseInt(e.target.id);
+        gameController.handleTurn(indexClicked);
+        displayBoard.render(gameBoard.array);
+        const result = gameController.getResult();
+        if (result !== "none"){
+            if (result === "player1"){
+                player1.incrementScore();
+            }else if (result === "player2"){
+                player2.incrementScore();
+            }
+
+            round++;
+            gameController.reset();
+            displayBoard.render(gameBoard.array);
+        }
+        updateDisplay();
+    });
+
+
+    buttons.addEventListener("click", e => {
+        if (e.target.id === "start"){
+        
+            gameController.reset();
+            round = 0;
+
+            const playerOneName = prompt("Player 1 Name: ");
+            const playerTwoName = prompt("Player 2 Name: ");
+
+            player1 = createPlayer(playerOneName);
+            player2 = createPlayer(playerTwoName);
+
+            gameStarted = true;
+            updateDisplay();
+
+        }else if (e.target.id === "reset"){
+
+            gameController.reset();
+            displayBoard.render(gameBoard.array);
+            round = 0;
+            updateDisplay();  
+        }
+    });
+
+
+    function updateDisplay() {
+
+        const players = document.querySelector("#players");
+        const roundRef = document.querySelector("#round");
+
+        if(gameController.isPlayerOne()){
+            players.textContent = "Player 1 Turn";
+        }else{
+            players.textContent = "Player 2 Turn";
+        }
+
+        roundRef.textContent = `Round: ${round} | ${player1.name} Score: ${player1.getScore()} | ${player2.name} Score: ${player2.getScore()}`;
+
+    }
+
+
 }
 
 
 
+displayBoard.render(gameBoard.array);
+setupEventListeners();
 
-// display array contents to board
 
 
 
